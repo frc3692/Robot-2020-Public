a85d76630325b486b0ab9b61cb5cbf48a74dfd47
Alex Linde <Amlinde@live.com>
2020-02-03 21:28:40 -0600

    Prepped trajectory following and LEDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,20 +7,48 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
     // Drivetrain constants
-    public class DriveConstants {
-        public static final int m_LeftFront = 1;
-        public static final int m_LeftBack = 2;
-        public static final int m_RightFront = 3;
-        public static final int m_RightBack = 4;
+    public static final class DriveConstants {
+        public static final int kFrontLeft = 1;
+        public static final int kBackLeft = 2;
+        public static final int kFrontRight = 3;
+        public static final int kBackRight = 4;
+
+        // Assuming it's the wheel Circumference
+        public static final double kWheelCirc = 0.1524 * Math.PI;
+
+        // Auto
+        // TODO: Get constants from frc robot characterization tool
+        public static final double ksVolts = 0;
+        public static final double ksVoltSecondsPerMeter = 0;
+        public static final double kaVoltSecondsSquaredPerMeter = 0;
+
+        public static final double KPDriveVel = 0;
+
+        public static final double kTrackWidthMeters = 0;
+        public static final DifferentialDriveKinematics kDriveConstants = new DifferentialDriveKinematics(
+                kTrackWidthMeters);
+
+        // Reasonable baseline values for a RAMSETE follower in units of meters and
+        // seconds
+        public static final double kRamseteB = 2;
+        public static final double kRamseteZeta = 0.7;
+    }
+
+    public static final class Misc {
+        public static final int kBlinkinPort = 0;
     }
 }
