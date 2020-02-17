306ad1ba6003d3fea333cfc742b34007f2f102d8
Alex Linde <amlinde@live.com>
2020-02-17 21:52:11 +0000

    Update IntakeLoop.java

--- src/main/java/frc/robot/commands/intake/IntakeLoop.java
+++ src/main/java/frc/robot/commands/intake/IntakeLoop.java
@@ -33,7 +33,7 @@ public class IntakeLoop extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_intake.set(m_speed.getAsDouble() * 0.6);
+    m_intake.set(m_speed.getAsDouble());
   }
 
   // Called once the command ends or is interrupted.
